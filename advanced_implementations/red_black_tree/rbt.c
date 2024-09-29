a9e1daae616a65a2f68fc1dd53c8af867326bc5e
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-09-29 02:09:59 +0200

    rbtree: set the coloring logic and the left rotation (right-right nodes)

--- advanced_implementations/red_black_tree/rbt.c
+++ advanced_implementations/red_black_tree/rbt.c
@@ -1,17 +1,21 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define RED 1
-#define BLACK 0
+#define RED false
+#define BLACK true
+#define ROOT 0
+#define LEFT 1
+#define RIGHT 2
 
 typedef struct RBNode RBNode;
 struct RBNode
 {
   int value;
-  int color;
-  RBNode *parent;
-  RBNode *left;
-  RBNode *right;
+  bool color;
+  int side;
+  RBNode *parent, *left, *right;
 };
 
 typedef struct
@@ -19,14 +23,39 @@ typedef struct
   RBNode *root;
 } RBTree;
 
+char *
+get_color (bool color)
+{
+  return color == RED ? "Red" : "Black";
+}
+char *
+get_side (int side)
+{
+  if (side == ROOT)
+    return "Root";
+  return side == LEFT ? "Left" : "Right";
+}
+
 void
-print_tree (RBNode *node)
+print_tree_helper (RBNode *node, int space, int indent_incr)
 {
   if (node == NULL)
     return;
-  print_tree (node->left);
-  printf (" %d ", node->value);
-  print_tree (node->right);
+
+  space += indent_incr;
+  print_tree_helper (node->right, space, indent_incr);
+  printf ("\n");
+  for (int i = indent_incr; i < space; i++)
+    printf (" ");
+  printf ("(%d,%s,%s)\n", node->value, get_color (node->color),
+          get_side (node->side));
+  print_tree_helper (node->left, space, indent_incr);
+}
+
+void
+print_tree (RBNode *root)
+{
+  print_tree_helper (root, 0, 7);
 }
 
 void
@@ -46,25 +75,133 @@ new_node (int value)
   RBNode *new_node = malloc (sizeof (RBNode));
   if (!new_node)
     return NULL;
-
   new_node->value = value;
   new_node->color = RED;
   new_node->left = NULL;
   new_node->right = NULL;
+  new_node->parent = NULL;
+  new_node->side = ROOT;
   return new_node;
 }
 
-RBNode *
-append_node (RBNode *node, int value)
+void
+left_rotate (RBNode *child, RBNode *parent, RBNode *grandparent, RBNode *uncle)
 {
-  if (node == NULL)
+  RBNode *tmp = grandparent->right;
+  grandparent->right = tmp->left;
+  if (grandparent->right != NULL)
+  {
+    grandparent->right->parent = grandparent;
+    grandparent->right->side = RIGHT;
+  }
+  if (!grandparent->parent) // This is the root position
+  {
+    tmp->parent = NULL;
+    tmp->side = ROOT;
+    grandparent = tmp;
+  }
+  else
+  {
+    tmp->parent = grandparent->parent;
+    if (grandparent->side == LEFT)
+    {
+      tmp->side = LEFT;
+      tmp->parent->left = tmp;
+    }
+    else if (grandparent->side == RIGHT)
+    {
+      tmp->side = RIGHT;
+      tmp->parent->right = tmp;
+    }
+  }
+  tmp->left = grandparent;
+  grandparent->side = LEFT;
+  grandparent->parent = tmp;
+}
+
+void
+rotate (RBNode *child, RBNode *parent, RBNode *grandparent, RBNode *uncle)
+{
+  // TODO: left left
+
+  // righ right
+  if (child->side == RIGHT && parent->side == RIGHT)
+  {
+    puts ("rr");
+    left_rotate (child, parent, grandparent, uncle);
+  }
+
+  // TODO: left right
+
+  // TODO: right left
+}
+
+void
+check_violations (RBNode *child)
+{
+  if (!child || !child->parent)
+    return;
+  RBNode *parent = child->parent;
+  RBNode *grandparent = parent->parent;
+  RBNode *uncle;
+  if (grandparent == NULL)
+    uncle = NULL;
+  else
+    uncle = parent->side == LEFT ? grandparent->right : grandparent->left;
+
+  if (parent->color == RED && (uncle == NULL || uncle->color == BLACK))
+  {
+    puts ("rotate");
+    rotate (child, parent, grandparent, uncle);
+  }
+  if (parent->color == RED && (uncle && uncle->color == RED))
+  {
+    puts ("color");
+    parent->color = BLACK;
+    uncle->color = BLACK;
+    if (grandparent->side != ROOT)
+      grandparent->color = RED;
+  }
+  check_violations (parent);
+}
+
+RBNode *
+append_node (RBNode *root, int value)
+{
+  if (root == NULL)
     return new_node (value);
 
-  if (value < node->value)
-    node->left = append_node (node->left, value);
-  if (value > node->value)
-    node->right = append_node (node->right, value);
-  return node;
+  RBNode *current = root;
+  RBNode *parent = NULL;
+
+  while (current != NULL)
+  {
+    parent = current;
+
+    if (value < current->value)
+      current = current->left;
+    else if (value > current->value)
+      current = current->right;
+    else
+      return root; // Value already exists in the tree
+  }
+
+  RBNode *new = new_node (value);
+  new->parent = parent;
+
+  if (value < parent->value)
+  {
+    new->side = LEFT;
+    parent->left = new;
+  }
+  else
+  {
+    new->side = RIGHT;
+    parent->right = new;
+  }
+
+  check_violations (new);
+  return root;
 }
 
 RBNode *
@@ -115,17 +252,24 @@ main (void)
 {
   RBTree tree = {0};
   tree.root = new_node (50);
+  tree.root->color = BLACK;
   append_node (tree.root, 40);
+  append_node (tree.root, 60);
+  append_node (tree.root, 12);
+  append_node (tree.root, 10);
+  append_node (tree.root, 9);
+  append_node (tree.root, 13);
   append_node (tree.root, 39);
   append_node (tree.root, 41);
   append_node (tree.root, 60);
   append_node (tree.root, 48);
   append_node (tree.root, 49);
   append_node (tree.root, 59);
+  append_node (tree.root, 58);
 
   // delete_node (tree.root, 60);
   // delete_node (tree.root, 40);
-  delete_node (tree.root, 40);
+  // delete_node (tree.root, 40);
   print_tree (tree.root);
   puts ("");
   printf ("smallest node: %d\n", find_min_value (tree.root)->value);
